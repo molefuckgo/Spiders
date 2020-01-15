c3e823e03a6f957f77b62b2fc466a36b740fad79
guowei <guowei@maetimes.com>
2020-01-15 21:24:39 +0800

    单线程耗时测试

--- Headline/HeadlineSpider.py
+++ Headline/HeadlineSpider.py
@@ -5,13 +5,30 @@
 @Time: 2020/1/9 1:24
 @Desc:
 """
+import asyncio
+import time
+
+import aiohttp
 import requests
 from constant import headers
 import ujson
-from concurrent.futures import  ThreadPoolExecutor
 
+
+async def get_article(item_id:int):
+    article_url = f'https://www.toutiao.com/i{item_id}/'
+    print(article_url)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=article_url, headers=headers) as resposne:
+            return await resposne.text()
+
+async def clean_article_data(item_id:int) -> None:
+    print('clean_article_data')
+    aritcle_result = await get_article(item_id=item_id)
+    print(aritcle_result)
+    # return aritcle_result
 
 def get_all_source_item_id(user_id:int) -> None:
+    loop = asyncio.get_event_loop()
     max_behot_time = 0
     all_item_id_list = list()
     has_more = True
@@ -20,6 +37,16 @@ def get_all_source_item_id(user_id:int) -> None:
         all_item_id_list += item_id_list
         print('user_id:', user_id)
     print(all_item_id_list)
+    tasks = [asyncio.ensure_future(clean_article_data(item_id=item_id)) for item_id in all_item_id_list]
+    print('tasks', tasks)
+    before_time = time.time()
+    loop.run_until_complete(asyncio.wait(tasks))
+    after_time = time.time()
+    print('协程共计耗时：', after_time - before_time)
+    for item_id in all_item_id_list:
+        article_url = f'https://www.toutiao.com/i{item_id}/'
+        requests.get(url=article_url, headers=headers)
+    print('单线程共计耗时：', time.time() - after_time)
 
 
 
@@ -38,8 +65,7 @@ def get_item_id_list_and_max_behot_time(user_id:int, max_behot_time:int) -> (lis
     return item_id_list, max_behot_time, has_more
 
 if __name__ == '__main__':
-    user_id_list = [5772853537, 110935079834]
-    pool = ThreadPoolExecutor(max_workers=5)
+    user_id_list = [5772853537]
     futures = list()
     for user_id in user_id_list:
-        futures.append(pool.submit(get_all_source_item_id, user_id))
+        get_all_source_item_id(user_id=user_id)
